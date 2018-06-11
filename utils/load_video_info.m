8a1a963e8c15a172dc2be69f346959dd0fea6aac
holy44162 <holy44162@gmail.com>
2018-06-11 14:41:51 +0800

    change the sequences' format.

--- utils/load_video_info.m
+++ utils/load_video_info.m
@@ -6,14 +6,25 @@ seq.format = 'otb';
 seq.len = size(ground_truth, 1);
 seq.init_rect = ground_truth(1,:);
 
-img_path = [video_path '/img/'];
+% img_path = [video_path '/img/'];
+img_path = [video_path '/imgs/'];
 
-if exist([img_path num2str(1, '%04i.png')], 'file'),
-    img_files = num2str((1:seq.len)', [img_path '%04i.png']);
-elseif exist([img_path num2str(1, '%04i.jpg')], 'file'),
-    img_files = num2str((1:seq.len)', [img_path '%04i.jpg']);
-elseif exist([img_path num2str(1, '%04i.bmp')], 'file'),
-    img_files = num2str((1:seq.len)', [img_path '%04i.bmp']);
+% if exist([img_path num2str(1, '%04i.png')], 'file'),
+%     img_files = num2str((1:seq.len)', [img_path '%04i.png']);
+% elseif exist([img_path num2str(1, '%04i.jpg')], 'file'),
+%     img_files = num2str((1:seq.len)', [img_path '%04i.jpg']);
+% elseif exist([img_path num2str(1, '%04i.bmp')], 'file'),
+%     img_files = num2str((1:seq.len)', [img_path '%04i.bmp']);
+% else
+%     error('No image files to load.')
+% end
+
+if exist([img_path num2str(1, 'img%05i.png')], 'file'),
+    img_files = num2str((1:seq.len)', [img_path 'img%05i.png']);
+elseif exist([img_path num2str(1, 'img%05i.jpg')], 'file'),
+    img_files = num2str((1:seq.len)', [img_path 'img%05i.jpg']);
+elseif exist([img_path num2str(1, 'img%05i.bmp')], 'file'),
+    img_files = num2str((1:seq.len)', [img_path 'img%05i.bmp']);
 else
     error('No image files to load.')
 end
