f24e286dc850cb0f110f7f0a8c8b44d4fd9bb9b8
holy44162 <holy44162@gmail.com>
2018-06-15 09:53:35 +0800

    add mess and total imgs.

--- annoInitialRect.m
+++ annoInitialRect.m
@@ -1,6 +1,8 @@
 addpath('d:\baiduSyn\files\phd\functions');
 
-folder_name = 'd:\data_seq\sequences\windingRope\imgs';
+% folder_name = 'd:\data_seq\sequences\windingRope\imgs';
+% folder_name = 'd:\data_seq\sequences\windingRopeMess\imgs';
+folder_name = 'd:\data_seq\sequences\windingRopeTotal\imgs';
 gtFilePathName = [folder_name(1:end-4) 'groundtruth_rect.txt'];
 if exist(gtFilePathName, 'file') == 2
     msgbox('groundtruth_rect.txt already exists.');
