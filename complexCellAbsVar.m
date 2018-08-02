f686eb6a478f3bd33f0d63e6b0e24c833e45ca09
holy44162 <holy44162@gmail.com>
2018-08-02 10:18:17 +0800

    add complex cell value to abs var function.

---
+++ complexCellAbsVar.m
@@ -0,0 +1,11 @@
+function [sumDiff,sumValue] = complexCellAbsVar(complexCell,indDirection)
+%UNTITLED 此处显示有关此函数的摘要
+%   此处显示详细说明
+testr2 = cellfun(@abs,complexCell,'UniformOutput',false);
+test1r2 = cellfun(@(x) sum(x(:)),testr2,'UniformOutput',false);
+test2r2 = cell2mat(test1r2);
+sumValue = sum(test2r2);
+sumDiff = bsxfun(@minus,sumValue(indDirection),sumValue);
+sumDiff(indDirection) = [];
+end
+
