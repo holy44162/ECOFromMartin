556c00a3c7e946c0b041ab2f236796c2f67732a5
James <James@DESKTOP-1A4AIF4>
2018-10-29 14:00:38 +0800

    complete script for rotate and rect.

--- testRotateAndRect.m
+++ testRotateAndRect.m
@@ -1,3 +1,61 @@
 functionPath = 'm:\files\files\phd\functions\';
 % functionPath = 'd:\baiduSyn\files\phd\functions\';
 addpath(functionPath);
+
+inputImg = 'd:\data_seq\smallWinding\train\imgs\img00001.jpg';
+
+img = imread(inputImg);
+
+figure,imshow(img);
+
+tagTestAngle = true;
+tagTestRect = true;
+theta = 0;
+
+while tagTestAngle
+    prompt = 'please input the rotate angle';
+    dlg_title = 'rotate angle';
+    num_lines = [1 70];
+    defaultans = {num2str(theta)};
+    optionsPrompt.WindowStyle = 'normal';
+    answer = inputdlg(prompt,dlg_title,num_lines,defaultans,optionsPrompt);
+    if isempty(answer)
+        val = theta;
+        tagTestAngle = false;
+        delete(gca);
+        close;
+        figure,imshow(imRotate);
+    else
+        val = str2double(answer{1,1});
+        theta = val;
+        tform = affine2d([cosd(theta) sind(theta) 0;...
+            -sind(theta) cosd(theta) 0; 0 0 1]);
+        imRotate = imwarp(img,tform);
+        delete(gca);
+        close;
+        figure,imshow(imRotate);
+    end
+end
+
+positionH = [10 10 50 50];
+while tagTestRect
+    h = imrect(gca, positionH);
+    positionH = wait(h);
+    delete(gca);
+    close;
+    figure,imshow(imRotate);
+    hold on;
+    rectangle('Position',positionH, 'EdgeColor','g', 'LineWidth',2);
+    hold off;
+    
+    answer = questdlg('Confirmed?');
+    if strcmpi(answer,'Yes')
+        tagTestRect = false;
+        delete(gca);
+        close;
+    else
+        delete(gca);
+        close;
+        figure,imshow(imRotate);
+    end
+end
